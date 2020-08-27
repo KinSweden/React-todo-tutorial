be344f74e3006f763efb27688727a1d33d32bd26
Karin <KinSweden@users.noreply.github.com>
2020-08-27 13:21:50 +0200

    Updates

---
+++ precache-manifest.0eedc778cb3dd4a577350a50b9d0b79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afbf12398841f123183e73aeaf269a7",
+    "url": "/github.io/React-todo-tutorial/index.html"
+  },
+  {
+    "revision": "d51a124baff98f42f6b7",
+    "url": "/github.io/React-todo-tutorial/static/css/main.9a7d16ca.chunk.css"
+  },
+  {
+    "revision": "171301a85693d7e3a7b0",
+    "url": "/github.io/React-todo-tutorial/static/js/2.baa2e1ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/React-todo-tutorial/static/js/2.baa2e1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51a124baff98f42f6b7",
+    "url": "/github.io/React-todo-tutorial/static/js/main.52138577.chunk.js"
+  },
+  {
+    "revision": "b5dbb3a1062fa23bbd84",
+    "url": "/github.io/React-todo-tutorial/static/js/runtime-main.3ea58688.js"
+  }
+]);
